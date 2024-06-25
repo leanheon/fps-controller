438b76189a24a20fb2e0e98bfef3471acc653cb6
leanheon <epogood58@naver.com>
2024-06-25 23:01:21 +0900

    Add files via upload

--- sending-part-code.ino
+++ sending-part-code.ino
@@ -1,80 +1,100 @@
+#include <Wire.h>
+#include <MPU6050.h>
 #include <SoftwareSerial.h>
-#include <Keyboard.h>
-#include <Mouse.h>
 
+MPU6050 mpu;
 SoftwareSerial bluetooth(2, 3); // RX, TX
 
-const int SENSITIVITY = 50; // Adjust as needed
-const int CENTER_BUTTON = 7; // Button to center the mouse
+const int JOYSTICK_X = A0;
+const int JOYSTICK_Y = A1;
+const int TRIGGER_BTN = 4;
+const int MAGAZINE_BTN = 5;
+const int HANDLE_BTN = 6;
+const int REAIM_BTN = 7;
+
+// Solenoid pins
+const int SOLENOID_PINS[] = {8, 9, 10, 11, 12, 13};
+const int SOLENOID_COUNT = 6;
+
+bool reloading = false;
+bool triggerHeld = false;
+unsigned long lastRecoilTime = 0;
+const int RECOIL_INTERVAL = 100; // Time between recoil activations in milliseconds
+const int SOLENOID_ACTIVATION_TIME = 20; // Solenoid activation time in milliseconds
 
 void setup() {
   Serial.begin(9600);
   bluetooth.begin(9600);
-  Keyboard.begin();
-  Mouse.begin();
-  pinMode(CENTER_BUTTON, INPUT_PULLUP);
+  
+  Wire.begin();
+  mpu.initialize();
+  
+  pinMode(TRIGGER_BTN, INPUT_PULLUP);
+  pinMode(MAGAZINE_BTN, INPUT_PULLUP);
+  pinMode(HANDLE_BTN, INPUT_PULLUP);
+  pinMode(REAIM_BTN, INPUT_PULLUP);
+  
+  for (int i = 0; i < SOLENOID_COUNT; i++) {
+    pinMode(SOLENOID_PINS[i], OUTPUT);
+    digitalWrite(SOLENOID_PINS[i], LOW);
+  }
 }
 
 void loop() {
-  if (bluetooth.available()) {
-    String data = bluetooth.readStringUntil('\n');
-    processData(data);
+  // Read gyroscope data
+  int16_t ax, ay, az, gx, gy, gz;
+  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+  
+  // Read joystick data
+  int joyX = analogRead(JOYSTICK_X);
+  int joyY = analogRead(JOYSTICK_Y);
+  
+  // Read button states
+  bool trigger = !digitalRead(TRIGGER_BTN);
+  bool magazine = !digitalRead(MAGAZINE_BTN);
+  bool handle = !digitalRead(HANDLE_BTN);
+  bool reaim = !digitalRead(REAIM_BTN);
+  
+  // Process reload
+  if (magazine && handle && !reloading) {
+    reloading = true;
+  } else if (!magazine && !handle) {
+    reloading = false;
   }
   
-  // Check for center button press
-  if (digitalRead(CENTER_BUTTON) == LOW) {
-    centerMouse();
+  // Process shooting and recoil
+  if (trigger && !reloading) {
+    triggerHeld = true;
+    if (millis() - lastRecoilTime >= RECOIL_INTERVAL) {
+      simulateRecoil();
+      lastRecoilTime = millis();
+    }
+  } else {
+    triggerHeld = false;
   }
+  
+  // Prepare data to send
+  String data = String(gx) + "," + String(gy) + "," + 
+                String(joyX) + "," + String(joyY) + "," +
+                String(trigger) + "," + String(reloading) + "," +
+                String(reaim);
+  
+  // Send data via Bluetooth
+  bluetooth.println(data);
+  
+  delay(10); // Adjust delay as needed for performance
 }
 
-void processData(String data) {
-  int gx, gy, joyX, joyY;
-  bool trigger, reloading, reaim;
-  
-  sscanf(data.c_str(), "%d,%d,%d,%d,%d,%d,%d", &gx, &gy, &joyX, &joyY, &trigger, &reloading, &reaim);
-  
-  // Process mouse movement
-  int mouseX = map(gx, -32768, 32767, -SENSITIVITY, SENSITIVITY);
-  int mouseY = map(gy, -32768, 32767, -SENSITIVITY, SENSITIVITY);
-  Mouse.move(mouseX, mouseY, 0);
-  
-  // Process WASD movement
-  if (joyY < 400) Keyboard.press('W');
-  else if (joyY > 600) Keyboard.press('S');
-  else {
-    Keyboard.release('W');
-    Keyboard.release('S');
+void simulateRecoil() {
+  // Activate all solenoids simultaneously
+  for (int i = 0; i < SOLENOID_COUNT; i++) {
+    digitalWrite(SOLENOID_PINS[i], HIGH);
   }
   
-  if (joyX < 400) Keyboard.press('A');
-  else if (joyX > 600) Keyboard.press('D');
-  else {
-    Keyboard.release('A');
-    Keyboard.release('D');
+  delay(SOLENOID_ACTIVATION_TIME); // Keep solenoids activated for the specified time
+  
+  // Deactivate all solenoids
+  for (int i = 0; i < SOLENOID_COUNT; i++) {
+    digitalWrite(SOLENOID_PINS[i], LOW);
   }
-  
-  // Process trigger
-  if (trigger) Mouse.press(MOUSE_LEFT);
-  else Mouse.release(MOUSE_LEFT);
-  
-  // Process reloading
-  if (reloading) Keyboard.press('R');
-  else Keyboard.release('R');
-  
-  // Process reaim
-  if (reaim) {
-    centerMouse();
-  }
-}
-
-void centerMouse() {
-  // Get the current screen resolution
-  // Note: This is a placeholder. You'll need to set these values manually
-  // based on the target computer's screen resolution.
-  int screenWidth = 1920;
-  int screenHeight = 1080;
-  
-  // Move the mouse to the center of the screen
-  Mouse.move((screenWidth / 2) - Mouse.getScreenWidth(), 
-             (screenHeight / 2) - Mouse.getScreenHeight());
 }
